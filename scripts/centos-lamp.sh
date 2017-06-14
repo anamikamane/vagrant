c5ac924c566c213f4ca0e4cc5cf32cfa5c46538d
Ajit jadhav <ajit.jadhav@os3infotech.com>
2017-06-14 18:23:57 +0530

    update

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
-
 yum update -y --exclude=kernel
-
 yum install -y vim git unzip screen
-
 yum install -y httpd httpd-devel httpd-tools
-
 sudo chkconfig --add httpd
 sudo chkconfig httpd on
 sudo service httpd stop
-
 sudo service httpd start
-
 sudo yum install -y php php-cli php-common php-devel php-mysql
-
 sudo yum install -y mysql mysql-server mysql-devel
-
 sudo chkconfig --add mysqld
 sudo chkconfig mysqld on
 sudo service mysqld start
-
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS dev_test";
 mysql -u root -e "SHOW DATABASES";
-
-
 cd vagrant
-
 sudo -u vagrant wget -q https://raw.githubusercontent.com/anamikamane/vagrant/master/file/index.html
-
 sudo -u vagrant wget -q https://raw.githubusercontent.com/anamikamane/vagrant/master/file/info.php
-
-
 service httpd restart
 
